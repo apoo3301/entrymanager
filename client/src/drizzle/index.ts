e60c295fa15899c8c323389eeb9b94e298612937
witchbl4des <menzo.smit-adam@epitech.eu>
2024-09-06 02:10:12 +0200

    chore: Update database connection string in drizzle/index.ts

--- client/src/drizzle/index.ts
+++ client/src/drizzle/index.ts
@@ -8,7 +8,7 @@ if (!DATABASE_URL) {
   throw new Error("No database connection string was provided to `neon()`.");
 }
 
-const sql = neon(DATABASE_URL);
+const sql = neon("postgresql://crm_owner:gtrcP6zifD7W@ep-quiet-resonance-a5n0k547.us-east-2.aws.neon.tech/crm?sslmode=require");
 
 const db = drizzle(sql, { schema });
 
